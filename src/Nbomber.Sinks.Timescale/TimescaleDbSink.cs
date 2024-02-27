4a698ac13468028068ce59e3e35869bd4ec01650
Andrey <111135827+mangystx@users.noreply.github.com>
2024-02-28 00:49:17 +0200

    add PointData.cs

--- src/Nbomber.Sinks.Timescale/TimescaleDbSink.cs
+++ src/Nbomber.Sinks.Timescale/TimescaleDbSink.cs
@@ -1,6 +1,40 @@
+using Microsoft.Extensions.Configuration;
+using NBomber.Contracts;
+using NBomber.Contracts.Stats;
+
 namespace NBomber.Sinks.Timescale;
 
-public class TimescaleDbSink
+public class TimescaleDbSink : IReportingSink
 {
+    public string SinkName { get; }
+
+    public Task Init(IBaseContext context, IConfiguration infraConfig)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Start()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task SaveRealtimeStats(ScenarioStats[] stats)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task SaveFinalStats(NodeStats stats)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Stop()
+    {
+        throw new NotImplementedException();
+    }
     
+    public void Dispose()
+    {
+        throw new NotImplementedException();
+    }
 }
