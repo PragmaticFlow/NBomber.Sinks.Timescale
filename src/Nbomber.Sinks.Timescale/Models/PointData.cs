6495c0e098b48251aeb0a33194fd6d9ff295393c
Andrey <111135827+mangystx@users.noreply.github.com>
2024-03-01 00:17:29 +0200

    crt state

--- src/Nbomber.Sinks.Timescale/Models/PointData.cs
+++ src/Nbomber.Sinks.Timescale/Models/PointData.cs
@@ -16,58 +16,58 @@ public class PointData
     public string Step { get; set; }
 
     // Fields
-    public int? ClusterNodeCount { get; set; }
-    public int? ClusterNodeCpuCount { get; set; }
+    public int ClusterNodeCount { get; set; }
+    public int ClusterNodeCpuCount { get; set; }
     
-    public string? SessionId { get; set; }
+    public string SessionId { get; set; }
 
-    public int? AllRequestCountAll { get; set; }
-    public long? AllDataTransferAll { get; set; }
+    public int AllRequestCountAll { get; set; }
+    public long AllDataTransferAll { get; set; }
 
-    public double? OkLatencyMax { get; set; }
-    public double? OkLatencyMean { get; set; }
-    public double? OkLatencyMin { get; set; }
-    public double? OkLatencyStdDev { get; set; }
-    public double? OkLatencyPercent50 { get; set; }
-    public double? OkLatencyPercent75 { get; set; }
-    public double? OkLatencyPercent95 { get; set; }
-    public double? OkLatencyPercent99 { get; set; }
+    public double OkLatencyMax { get; set; }
+    public double OkLatencyMean { get; set; }
+    public double OkLatencyMin { get; set; }
+    public double OkLatencyStdDev { get; set; }
+    public double OkLatencyPercent50 { get; set; }
+    public double OkLatencyPercent75 { get; set; }
+    public double OkLatencyPercent95 { get; set; }
+    public double OkLatencyPercent99 { get; set; }
 
-    public long? OkDataTransferMin { get; set; }
-    public long? OkDataTransferMean { get; set; }
-    public long? OkDataTransferMax { get; set; }
-    public long? OkDataTransferAll { get; set; }
-    public long? OkDataTransferPercent50 { get; set; }
-    public long? OkDataTransferPercent75 { get; set; }
-    public long? OkDataTransferPercent95 { get; set; }
-    public long? OkDataTransferPercent99 { get; set; }
+    public long OkDataTransferMin { get; set; }
+    public long OkDataTransferMean { get; set; }
+    public long OkDataTransferMax { get; set; }
+    public long OkDataTransferAll { get; set; }
+    public long OkDataTransferPercent50 { get; set; }
+    public long OkDataTransferPercent75 { get; set; }
+    public long OkDataTransferPercent95 { get; set; }
+    public long OkDataTransferPercent99 { get; set; }
 
-    public int? FailRequestCount { get; set; }
-    public double? FailRequestRps { get; set; }
+    public int FailRequestCount { get; set; }
+    public double FailRequestRps { get; set; }
     
-    public double? FailLatencyMax { get; set; }
-    public double? FailLatencyMean { get; set; }
-    public double? FailLatencyMin { get; set; }
-    public double? FailLatencyStdDev { get; set; }
-    public double? FailLatencyPercent50 { get; set; }
-    public double? FailLatencyPercent75 { get; set; }
-    public double? FailLatencyPercent95 { get; set; }
-    public double? FailLatencyPercent99 { get; set; }
+    public double FailLatencyMax { get; set; }
+    public double FailLatencyMean { get; set; }
+    public double FailLatencyMin { get; set; }
+    public double FailLatencyStdDev { get; set; }
+    public double FailLatencyPercent50 { get; set; }
+    public double FailLatencyPercent75 { get; set; }
+    public double FailLatencyPercent95 { get; set; }
+    public double FailLatencyPercent99 { get; set; }
 
-    public long? FailDataTransferMin { get; set; }
-    public long? FailDataTransferMean { get; set; }
-    public long? FailDataTransferMax { get; set; }
-    public long? FailDataTransferAll { get; set; }
-    public long? FailDataTransferPercent50 { get; set; }
-    public long? FailDataTransferPercent75 { get; set; }
-    public long? FailDataTransferPercent95 { get; set; }
-    public long? FailDataTransferPercent99 { get; set; }
+    public long FailDataTransferMin { get; set; }
+    public long FailDataTransferMean { get; set; }
+    public long FailDataTransferMax { get; set; }
+    public long FailDataTransferAll { get; set; }
+    public long FailDataTransferPercent50 { get; set; }
+    public long FailDataTransferPercent75 { get; set; }
+    public long FailDataTransferPercent95 { get; set; }
+    public long FailDataTransferPercent99 { get; set; }
     
-    public int? SimulationValue { get; set; }
+    public int SimulationValue { get; set; }
 
-    public int? LatencyCountLessOrEq800 { get; set; }
-    public int? LatencyCountMore800Less1200 { get; set; }
-    public int? LatencyCountMoreOrEq1200 { get; set; }
+    public int LatencyCountLessOrEq800 { get; set; }
+    public int LatencyCountMore800Less1200 { get; set; }
+    public int LatencyCountMoreOrEq1200 { get; set; }
 
-    public int? StatusCodeCount { get; set; }
+    public int StatusCodeCount { get; set; }
 }
--- src/Nbomber.Sinks.Timescale/TimescaleDbSink.cs
+++ src/Nbomber.Sinks.Timescale/TimescaleDbSink.cs
@@ -1,21 +1,74 @@
+using Serilog;
 using Microsoft.Extensions.Configuration;
 using NBomber.Contracts;
 using NBomber.Contracts.Stats;
+using NBomber.Sinks.Timescale.Models;
+using NBomber.Sinks.Timescale.Structures;
 
 namespace NBomber.Sinks.Timescale;
 
+public class TimescaleDbSinkConfig
+{
+    public string ConnectionString { get; set; }
+}
+
+
 public class TimescaleDbSink : IReportingSink
 {
-    public string SinkName { get; }
+    private ILogger _logger;
+    private IBaseContext _context;
+    private TimescaleDbContext _timescaleDbContext;
+    
+    public string SinkName => "NBomber.Sinks.TimescaleDb";
 
+    public TimescaleDbSink() { }
+
+    public TimescaleDbSink(string connectionString)
+    {
+        _timescaleDbContext = new TimescaleDbContext(connectionString);
+    }
+    
+    public TimescaleDbSink(TimescaleDbContext dbContext)
+    {
+        _timescaleDbContext = dbContext;
+    }
+    
     public Task Init(IBaseContext context, IConfiguration infraConfig)
     {
-        throw new NotImplementedException();
+        _logger = context.Logger.ForContext<TimescaleDbSink>();
+        _context = context;
+
+        var config = infraConfig?.GetSection("InfluxDBSink").Get<TimescaleDbSinkConfig>();
+        if (config != null)
+        {
+            _timescaleDbContext = new TimescaleDbContext(config.ConnectionString);
+        }
+        
+        if (_timescaleDbContext == null)
+        {
+            _logger.Error("Reporting Sink {0} has problems with initialization. The problem could be related to invalid config structure.", SinkName);
+                
+            throw new Exception(
+                $"Reporting Sink {SinkName} has problems with initialization. The problem could be related to invalid config structure.");
+        }
+            
+        return Task.CompletedTask;
     }
 
     public Task Start()
     {
-        throw new NotImplementedException();
+        if (_timescaleDbContext != null)
+        {
+            var point = new PointData
+            {
+                Measurement = "nbomber",
+                
+                ClusterNodeCount = 1,
+                ClusterNodeCpuCount = _context.GetNodeInfo().CoresCount,
+                
+                
+            };
+        }
     }
 
     public Task SaveRealtimeStats(ScenarioStats[] stats)
@@ -36,5 +89,12 @@ public class TimescaleDbSink : IReportingSink
     public void Dispose()
     {
         throw new NotImplementedException();
+    }
+
+    private void AddTestInfoTags(PointData point)
+    {
+        var nodeInfo = _context.GetNodeInfo();
+        var testInfo = _context.TestInfo;
+        
     }
 }
