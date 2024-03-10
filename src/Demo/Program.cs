3f2539c5ff0c60af87cf0bdfd82ba28c50bc6219
Andrey <111135827+mangystx@users.noreply.github.com>
2024-03-10 03:16:33 +0200

    x

--- src/Demo/Program.cs
+++ src/Demo/Program.cs
@@ -1,49 +1,65 @@
-﻿using NBomber.CSharp;
+﻿// using NBomber.CSharp;
+// using NBomber.Sinks.Timescale;
+// using Serilog;
+//
+// new TimescaleDBReportingExample().Run();
+//
+// public class TimescaleDBReportingExample
+// {
+//     private readonly TimescaleDbSink _timescaleDbSink = 
+//         new("Host=localhost;Port=5432;Database=timescaledb;Username=andrii;Password=myPass;Pooling=true;");
+//     
+//     public void Run()
+//     {
+//         var scenario = Scenario.Create("user_flow_scenario", async context =>
+//             {
+//                 var step1 = await Step.Run("login", context, async () =>
+//                 {
+//                     await Task.Delay(500);
+//                     return Response.Ok(sizeBytes: 10, statusCode: "200");
+//                 });
+//
+//                 var step2 = await Step.Run("get_product", context, async () =>
+//                 {
+//                     await Task.Delay(1000);
+//                     return Response.Ok(sizeBytes: 20, statusCode: "200");
+//                 });
+//
+//                 var step3 = await Step.Run("buy_product", context, async () =>
+//                 {
+//                     await Task.Delay(2000);
+//                     return Response.Ok(sizeBytes: 30, statusCode: "200");
+//                 });
+//
+//                 return Response.Ok(statusCode: "201");
+//             })
+//             .WithLoadSimulations(
+//                 Simulation.KeepConstant(1, TimeSpan.FromSeconds(30))
+//             ).WithoutWarmUp();
+//
+//         NBomberRunner
+//             .RegisterScenarios(scenario)
+//             //.LoadInfraConfig("infra-config.json")
+//             .WithReportingInterval(TimeSpan.FromSeconds(5))
+//             .WithReportingSinks(_timescaleDbSink)
+//             .WithTestSuite("reporting")
+//             .WithTestName("timescale_db_demo")
+//             .Run();
+//     }
+// }
+
+using Dapper;
 using NBomber.Sinks.Timescale;
-using Serilog;
+using Npgsql;
 
-new TimescaleDBReportingExample().Run();
+using var _connection = new NpgsqlConnection(
+    "Host=localhost;Port=5432;Database=timescaledb;Username=andrii;Password=myPass;Pooling=true;");
 
-public class TimescaleDBReportingExample
-{
-    private readonly TimescaleDbSink _timescaleDbSink = 
-        new("Host=localhost;Port=5432;Database=timescaledb;Username=andrii;Password=myPass;Pooling=true;");
+_connection.Open();
+
+Task.WhenAll(_connection.ExecuteAsync(SqlQueries.CreatePointDataStartTable),
+    _connection.ExecuteAsync(SqlQueries.CreatePointDataStatusCodesTable),
+    _connection.ExecuteAsync(SqlQueries.CreatePointDataLatencyCountsTable),
+    _connection.ExecuteAsync(SqlQueries.CreatePointDataStepStatsTable)); 
     
-    public void Run()
-    {
-        var scenario = Scenario.Create("user_flow_scenario", async context =>
-            {
-                var step1 = await Step.Run("login", context, async () =>
-                {
-                    await Task.Delay(500);
-                    return Response.Ok(sizeBytes: 10, statusCode: "200");
-                });
-
-                var step2 = await Step.Run("get_product", context, async () =>
-                {
-                    await Task.Delay(1000);
-                    return Response.Ok(sizeBytes: 20, statusCode: "200");
-                });
-
-                var step3 = await Step.Run("buy_product", context, async () =>
-                {
-                    await Task.Delay(2000);
-                    return Response.Ok(sizeBytes: 30, statusCode: "200");
-                });
-
-                return Response.Ok(statusCode: "201");
-            })
-            .WithLoadSimulations(
-                Simulation.KeepConstant(1, TimeSpan.FromSeconds(30))
-            ).WithoutWarmUp();
-
-        NBomberRunner
-            .RegisterScenarios(scenario)
-            //.LoadInfraConfig("infra-config.json")
-            .WithReportingInterval(TimeSpan.FromSeconds(5))
-            .WithReportingSinks(_timescaleDbSink)
-            .WithTestSuite("reporting")
-            .WithTestName("timescale_db_demo")
-            .Run();
-    }
-}
+    _connection.Close();
--- src/Nbomber.Sinks.Timescale/SqlQueries.cs
+++ src/Nbomber.Sinks.Timescale/SqlQueries.cs
@@ -1,6 +1,108 @@
+using NBomber.Sinks.Timescale.Models;
+
 namespace NBomber.Sinks.Timescale;
 
 public class SqlQueries
 {
+     public static string CreatePointDataLatencyCountsTable => $@"
+        CREATE TABLE IF NOT EXISTS latency_counts_points
+        (
+            {nameof(PointDataBase.Measurement)} TEXT,
+            {nameof(PointDataBase.Time)} TIMESTAMPTZ,
+            {nameof(PointDataBase.SessionId)} TEXT,
+            {nameof(PointDataBase.CurrentOperation)} TEXT,
+            {nameof(PointDataBase.NodeType)} TEXT,
+            {nameof(PointDataBase.TestSuite)} TEXT,
+            {nameof(PointDataBase.TestName)} TEXT,
+            {nameof(PointDataBase.ClusterId)} TEXT,
+            {nameof(PointDataLatencyCounts.Scenario)} TEXT,
+            {nameof(PointDataLatencyCounts.LatencyCountLessOrEq800)} INT,
+            {nameof(PointDataLatencyCounts.LatencyCountMore800Less1200)} INT,
+            {nameof(PointDataLatencyCounts.LatencyCountMoreOrEq1200)} INT
+        );";
+
+    public static string CreatePointDataStartTable => $@"
+        CREATE TABLE IF NOT EXISTS start_points
+        (
+            {nameof(PointDataBase.Measurement)} TEXT,
+            {nameof(PointDataBase.Time)} TIMESTAMPTZ,
+            {nameof(PointDataBase.SessionId)} TEXT,
+            {nameof(PointDataBase.CurrentOperation)} TEXT,
+            {nameof(PointDataBase.NodeType)} TEXT,
+            {nameof(PointDataBase.TestSuite)} TEXT,
+            {nameof(PointDataBase.TestName)} TEXT,
+            {nameof(PointDataBase.ClusterId)} TEXT,
+            {nameof(PointDataStart.ClusterNodeCount)} INT,
+            {nameof(PointDataStart.ClusterNodeCpuCount)} INT
+        );";
+
+    public static string CreatePointDataStatusCodesTable => $@"
+        CREATE TABLE IF NOT EXISTS status_codes_points
+        (
+            {nameof(PointDataBase.Measurement)} TEXT,
+            {nameof(PointDataBase.Time)} TIMESTAMPTZ,
+            {nameof(PointDataBase.SessionId)} TEXT,
+            {nameof(PointDataBase.CurrentOperation)} TEXT,
+            {nameof(PointDataBase.NodeType)} TEXT,
+            {nameof(PointDataBase.TestSuite)} TEXT,
+            {nameof(PointDataBase.TestName)} TEXT,
+            {nameof(PointDataBase.ClusterId)} TEXT,
+            {nameof(PointDataStatusCodes.Scenario)} TEXT,
+            {nameof(PointDataStatusCodes.StatusCodeStatus)} TEXT,
+            {nameof(PointDataStatusCodes.StatusCodeCount)} INT
+        );";
     
+    public static string CreatePointDataStepStatsTable => $@"
+    CREATE TABLE IF NOT EXISTS step_stats_points
+    (
+        {nameof(PointDataBase.Measurement)} TEXT,
+        {nameof(PointDataBase.Time)} TIMESTAMPTZ,
+        {nameof(PointDataBase.SessionId)} TEXT,
+        {nameof(PointDataBase.CurrentOperation)} TEXT,
+        {nameof(PointDataBase.NodeType)} TEXT,
+        {nameof(PointDataBase.TestSuite)} TEXT,
+        {nameof(PointDataBase.TestName)} TEXT,
+        {nameof(PointDataBase.ClusterId)} TEXT,
+        {nameof(PointDataStepStats.Scenario)} TEXT,
+        {nameof(PointDataStepStats.AllRequestCount)} INT,
+        {nameof(PointDataStepStats.AllDataTransferAll)} BIGINT,
+        {nameof(PointDataStepStats.OkRequestCount)} INT,
+        {nameof(PointDataStepStats.OkRequestRps)} DOUBLE PRECISION,
+        {nameof(PointDataStepStats.OkLatencyMax)} DOUBLE PRECISION,
+        {nameof(PointDataStepStats.OkLatencyMean)} DOUBLE PRECISION,
+        {nameof(PointDataStepStats.OkLatencyMin)} DOUBLE PRECISION,
+        {nameof(PointDataStepStats.OkLatencyStdDev)} DOUBLE PRECISION,
+        {nameof(PointDataStepStats.OkLatencyPercent50)} DOUBLE PRECISION,
+        {nameof(PointDataStepStats.OkLatencyPercent75)} DOUBLE PRECISION,
+        {nameof(PointDataStepStats.OkLatencyPercent95)} DOUBLE PRECISION,
+        {nameof(PointDataStepStats.OkLatencyPercent99)} DOUBLE PRECISION,
+        {nameof(PointDataStepStats.OkDataTransferMin)} BIGINT,
+        {nameof(PointDataStepStats.OkDataTransferMean)} BIGINT,
+        {nameof(PointDataStepStats.OkDataTransferMax)} BIGINT,
+        {nameof(PointDataStepStats.OkDataTransferAll)} BIGINT,
+        {nameof(PointDataStepStats.OkDataTransferPercent50)} BIGINT,
+        {nameof(PointDataStepStats.OkDataTransferPercent75)} BIGINT,
+        {nameof(PointDataStepStats.OkDataTransferPercent95)} BIGINT,
+        {nameof(PointDataStepStats.OkDataTransferPercent99)} BIGINT,
+        {nameof(PointDataStepStats.FailRequestCount)} INT,
+        {nameof(PointDataStepStats.FailRequestRps)} DOUBLE PRECISION,
+        {nameof(PointDataStepStats.FailLatencyMax)} DOUBLE PRECISION,
+        {nameof(PointDataStepStats.FailLatencyMean)} DOUBLE PRECISION,
+        {nameof(PointDataStepStats.FailLatencyMin)} DOUBLE PRECISION,
+        {nameof(PointDataStepStats.FailLatencyStdDev)} DOUBLE PRECISION,
+        {nameof(PointDataStepStats.FailLatencyPercent50)} DOUBLE PRECISION,
+        {nameof(PointDataStepStats.FailLatencyPercent75)} DOUBLE PRECISION,
+        {nameof(PointDataStepStats.FailLatencyPercent95)} DOUBLE PRECISION,
+        {nameof(PointDataStepStats.FailLatencyPercent99)} DOUBLE PRECISION,
+        {nameof(PointDataStepStats.FailDataTransferMin)} BIGINT,
+        {nameof(PointDataStepStats.FailDataTransferMean)} BIGINT,
+        {nameof(PointDataStepStats.FailDataTransferMax)} BIGINT,
+        {nameof(PointDataStepStats.FailDataTransferAll)} BIGINT,
+        {nameof(PointDataStepStats.FailDataTransferPercent50)} BIGINT,
+        {nameof(PointDataStepStats.FailDataTransferPercent75)} BIGINT,
+        {nameof(PointDataStepStats.FailDataTransferPercent95)} BIGINT,
+        {nameof(PointDataStepStats.FailDataTransferPercent99)} BIGINT,
+        {nameof(PointDataStepStats.SimulationValue)} INT
+    );";
+
 }
