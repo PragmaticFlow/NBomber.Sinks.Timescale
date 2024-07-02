54ccd4b35f999ca27776a4aa4c10309775d725ea
antyadev <antyadev@gmail.com>
2024-07-02 09:33:11 +0300

    Converted main data to normal columns, and (StatusCodes, LatencyCount) to JSONB

--- src/NBomber.Sinks.Timescale/Contracts/PointDbRecord.cs
+++ src/NBomber.Sinks.Timescale/Contracts/PointDbRecord.cs
@@ -10,12 +10,58 @@ public class PointDbRecord
     [Column(ColumnNames.Time)] public DateTimeOffset Time { get; set; }
     [Column(ColumnNames.SessionId)] public string SessionId { get; set; }
     [Column(ColumnNames.CurrentOperation)] public OperationType CurrentOperation { get; set; }
+    
     [Column(ColumnNames.TestSuite)] public string TestSuite { get; set; }
     [Column(ColumnNames.TestName)] public string TestName { get; set; }
     [Column(ColumnNames.Scenario)] public string Scenario { get; set; }
     [Column(ColumnNames.Step)] public string Step { get; set; }
-    [Column(ColumnNames.OkStepStats)] public string OkStepStats { get; set; }
-    [Column(ColumnNames.FailStepStats)] public string FailStepStats { get; set; }
+    
+    [Column(ColumnNames.AllReqCount)] public int AllReqCount { get; set; }
+    [Column(ColumnNames.AllDataAll)] public long AllDataAll { get; set; }
+    
+    [Column(ColumnNames.OkReqCount)] public int OkReqCount { get; set; }
+    [Column(ColumnNames.OkReqRps)] public double OkReqRps { get; set; }
+    [Column(ColumnNames.OkLatencyMax)] public double OkLatencyMax { get; set; }
+    [Column(ColumnNames.OkLatencyMean)] public double OkLatencyMean { get; set; }
+    [Column(ColumnNames.OkLatencyMin)] public double OkLatencyMin { get; set; }
+    [Column(ColumnNames.OkLatencyStdDev)] public double OkLatencyStdDev { get; set; }
+    [Column(ColumnNames.OkLatencyP50)] public double OkLatencyP50 { get; set; }
+    [Column(ColumnNames.OkLatencyP75)] public double OkLatencyP75 { get; set; }
+    [Column(ColumnNames.OkLatencyP95)] public double OkLatencyP95 { get; set; }
+    [Column(ColumnNames.OkLatencyP99)] public double OkLatencyP99 { get; set; }
+    [Column(ColumnNames.OkDataMin)] public long OkDataMin { get; set; }
+    [Column(ColumnNames.OkDataMean)] public long OkDataMean { get; set; }
+    [Column(ColumnNames.OkDataMax)] public long OkDataMax { get; set; }
+    [Column(ColumnNames.OkDataAll)] public long OkDataAll { get; set; }
+    [Column(ColumnNames.OkDataP50)] public long OkDataP50 { get; set; }
+    [Column(ColumnNames.OkDataP75)] public long OkDataP75 { get; set; }
+    [Column(ColumnNames.OkDataP95)] public long OkDataP95 { get; set; }
+    [Column(ColumnNames.OkDataP99)] public long OkDataP99 { get; set; }
+    [Column(ColumnNames.OkStatusCodes)] public string OkStatusCodes { get; set; }
+    [Column(ColumnNames.OkLatencyCount)] public string OkLatencyCount { get; set; }
+    
+    [Column(ColumnNames.FailReqCount)] public int FailReqCount { get; set; }
+    [Column(ColumnNames.FailReqRps)] public double FailReqRps { get; set; }
+    [Column(ColumnNames.FailLatencyMax)] public double FailLatencyMax { get; set; }
+    [Column(ColumnNames.FailLatencyMean)] public double FailLatencyMean { get; set; }
+    [Column(ColumnNames.FailLatencyMin)] public double FailLatencyMin { get; set; }
+    [Column(ColumnNames.FailLatencyStdDev)] public double FailLatencyStdDev { get; set; }
+    [Column(ColumnNames.FailLatencyP50)] public double FailLatencyP50 { get; set; }
+    [Column(ColumnNames.FailLatencyP75)] public double FailLatencyP75 { get; set; }
+    [Column(ColumnNames.FailLatencyP95)] public double FailLatencyP95 { get; set; }
+    [Column(ColumnNames.FailLatencyP99)] public double FailLatencyP99 { get; set; }
+    [Column(ColumnNames.FailDataMin)] public long FailDataMin { get; set; }
+    [Column(ColumnNames.FailDataMean)] public long FailDataMean { get; set; }
+    [Column(ColumnNames.FailDataMax)] public long FailDataMax { get; set; }
+    [Column(ColumnNames.FailDataAll)] public long FailDataAll { get; set; }
+    [Column(ColumnNames.FailDataP50)] public long FailDataP50 { get; set; }
+    [Column(ColumnNames.FailDataP75)] public long FailDataP75 { get; set; }
+    [Column(ColumnNames.FailDataP95)] public long FailDataP95 { get; set; }
+    [Column(ColumnNames.FailDataP99)] public long FailDataP99 { get; set; }
+    [Column(ColumnNames.FailStatusCodes)] public string FailStatusCodes { get; set; }
+    [Column(ColumnNames.FailLatencyCount)] public string FailLatencyCount { get; set; }
+    
+    [Column(ColumnNames.SimulationValue)] public int SimulationValue { get; set; }
 }
 
 public class NodeInfoDbRecord
--- src/NBomber.Sinks.Timescale/SqlQueries.cs
+++ src/NBomber.Sinks.Timescale/SqlQueries.cs
@@ -14,13 +14,57 @@ public static class SqlQueries
         (
             ""{ColumnNames.Time}"" TIMESTAMPTZ NOT NULL,
             ""{ColumnNames.SessionId}"" TEXT NOT NULL,
-            ""{ColumnNames.CurrentOperation}"" TEXT NOT NULL,            
-            ""{ColumnNames.TestSuite}"" TEXT NOT NULL,
-            ""{ColumnNames.TestName}"" TEXT NOT NULL,
-            ""{ColumnNames.Scenario}"" TEXT NOT NULL,
-            ""{ColumnNames.Step}"" TEXT NOT NULL,            
-            ""{ColumnNames.OkStepStats}"" JSONB NOT NULL,
-            ""{ColumnNames.FailStepStats}"" JSONB NOT NULL
+            ""{ColumnNames.CurrentOperation}"" TEXT,            
+            
+            ""{ColumnNames.TestSuite}"" TEXT,
+            ""{ColumnNames.TestName}"" TEXT,
+            ""{ColumnNames.Scenario}"" TEXT,
+            ""{ColumnNames.Step}"" TEXT,            
+            
+            ""{ColumnNames.AllReqCount}"" INT,
+            ""{ColumnNames.AllDataAll}"" BIGINT,
+            
+            ""{ColumnNames.OkReqCount}"" INT,
+            ""{ColumnNames.OkReqRps}"" DOUBLE PRECISION,
+            ""{ColumnNames.OkLatencyMax}"" DOUBLE PRECISION,
+            ""{ColumnNames.OkLatencyMean}"" DOUBLE PRECISION,
+            ""{ColumnNames.OkLatencyMin}"" DOUBLE PRECISION,
+            ""{ColumnNames.OkLatencyStdDev}"" DOUBLE PRECISION,
+            ""{ColumnNames.OkLatencyP50}"" DOUBLE PRECISION,
+            ""{ColumnNames.OkLatencyP75}"" DOUBLE PRECISION,
+            ""{ColumnNames.OkLatencyP95}"" DOUBLE PRECISION,
+            ""{ColumnNames.OkLatencyP99}"" DOUBLE PRECISION,
+            ""{ColumnNames.OkDataMin}"" BIGINT,
+            ""{ColumnNames.OkDataMean}"" BIGINT,
+            ""{ColumnNames.OkDataMax}"" BIGINT,
+            ""{ColumnNames.OkDataAll}"" BIGINT,
+            ""{ColumnNames.OkDataP50}"" BIGINT,
+            ""{ColumnNames.OkDataP75}"" BIGINT,
+            ""{ColumnNames.OkDataP95}"" BIGINT,
+            ""{ColumnNames.OkDataP99}"" BIGINT,
+            ""{ColumnNames.OkStatusCodes}"" JSONB,
+            ""{ColumnNames.OkLatencyCount}"" JSONB,            
+            
+            ""{ColumnNames.FailReqCount}"" INT,
+            ""{ColumnNames.FailReqRps}"" DOUBLE PRECISION, 
+            ""{ColumnNames.FailLatencyMax}"" DOUBLE PRECISION,
+            ""{ColumnNames.FailLatencyMean}"" DOUBLE PRECISION,
+            ""{ColumnNames.FailLatencyMin}"" DOUBLE PRECISION,
+            ""{ColumnNames.FailLatencyStdDev}"" DOUBLE PRECISION,
+            ""{ColumnNames.FailLatencyP50}"" DOUBLE PRECISION,
+            ""{ColumnNames.FailLatencyP75}"" DOUBLE PRECISION,
+            ""{ColumnNames.FailLatencyP95}"" DOUBLE PRECISION,
+            ""{ColumnNames.FailLatencyP99}"" DOUBLE PRECISION,
+            ""{ColumnNames.FailDataMin}"" BIGINT,
+            ""{ColumnNames.FailDataMean}"" BIGINT,
+            ""{ColumnNames.FailDataMax}"" BIGINT,
+            ""{ColumnNames.FailDataAll}"" BIGINT,
+            ""{ColumnNames.FailDataP50}"" BIGINT,
+            ""{ColumnNames.FailDataP75}"" BIGINT,
+            ""{ColumnNames.FailDataP95}"" BIGINT,
+            ""{ColumnNames.FailDataP99}"" BIGINT,
+            
+            ""{ColumnNames.SimulationValue}"" INT
         );
         SELECT create_hypertable('{StepStatsTable}', by_range('{ColumnNames.Time}', INTERVAL '1 day'), if_not_exists => TRUE);
         CREATE INDEX IF NOT EXISTS {ColumnNames.SessionId}_index ON {StepStatsTable} ({ColumnNames.SessionId});
@@ -31,7 +75,7 @@ public static class SqlQueries
         (            
             ""{ColumnNames.Time}"" TIMESTAMPTZ NOT NULL,
             ""{ColumnNames.SessionId}"" TEXT NOT NULL,            
-            ""{ColumnNames.NodeInfo}"" JSONB NOT NULL            
+            ""{ColumnNames.NodeInfo}"" JSONB            
         );
         SELECT create_hypertable('{ClusterStatsTableName}', by_range('{ColumnNames.Time}', INTERVAL '1 day'), if_not_exists => TRUE);
         CREATE INDEX IF NOT EXISTS {ColumnNames.SessionId}_index ON {ClusterStatsTableName} ({ColumnNames.SessionId});
