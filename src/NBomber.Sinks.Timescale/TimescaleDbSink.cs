e00cbf6d2165631194786b21afdc70245c4d17b6
sanirr <oleksandr.m.dev@gmail.com>
2026-07-20 14:17:40 +0300

    Restore the session_id parameter in the `SaveFinalStats` method

--- src/NBomber.Sinks.Timescale/TimescaleDbSink.cs
+++ src/NBomber.Sinks.Timescale/TimescaleDbSink.cs
@@ -221,6 +221,7 @@ public class TimescaleDbSink : IReportingSink
 
         var queryEntity = new SessionInfoDbRecord
         {
+            SessionId = testInfo.SessionId,
             CurrentOperation = stats.NodeInfo.CurrentOperation,
             LastUpdatedTime = currentTime,
             SessionResult = JsonSerializer.Serialize(new SessionResult(stepsStats)),
@@ -233,7 +234,7 @@ public class TimescaleDbSink : IReportingSink
         await connection.BinaryBulkInsertAsync(TableNames.StepStatsTable, stepsStats, transaction: ts);
         await connection.BinaryBulkInsertAsync(TableNames.MetricsTable, metrics, transaction: ts);
 
-        var fields = Field.Parse<SessionInfoDbRecord>(e => new { e.CurrentOperation, e.LastUpdatedTime, e.SessionResult, e.Artifacts });
+        var fields = Field.Parse<SessionInfoDbRecord>(e => new { e.SessionId, e.CurrentOperation, e.LastUpdatedTime, e.SessionResult, e.Artifacts });
         await connection.UpdateAsync(TableNames.SessionsTable, queryEntity, fields: fields, transaction: ts);
 
         await ts.CommitAsync();
